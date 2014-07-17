42071cc53fb1c28939b5912fb46c561b56eb9dd6
Ilir Maci <ilir.maci@gmail.com>
2014-07-17 23:58:01 +0200

    completed part 3 assignment 1

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,4 +1,38 @@
 import sys
+import json
+
+def get_scored_terms(infile):
+    ''' (file open for reading) -> dict of {str:int}
+    Return dictionary from word list and associated
+    sentiment score in infile.
+    '''
+    # read the file with scores into a dictionary
+    scores = {}  ## initialize empty dictionary
+    for line in infile:
+        term, score = line.split("\t")  ## parse TAB-delimited file
+        scores[term] = int(score)  ## add key-value pair
+    
+    return scores
+
+
+def clean_text(text):
+    ''' (str) -> str
+    Return text after keeping only letters and the
+    space character.
+    '''
+    import string  ## for list of letters
+    
+    # split to characters and replace non-letter 
+    # characters with space
+    letter_list = [c if c in string.letters else " " for c in text]
+    
+    # join again into a string
+    text = "".join(letter_list)
+        
+    # change everything to lowercase and return
+    return text.lower()
+
+
 
 def hw():
     print 'Hello, world!'
@@ -9,9 +43,55 @@ def lines(fp):
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    
+    # read the file with scores into a dictionary
+    score = get_scored_terms(sent_file)
+    
+    # initialize dictionaries for positive and negative word counts
+    positive = {}
+    negative = {}
+    
+    # parse the tweet file line by line
+    # and build positive and negative word counts
+    for line in tweet_file:
+        tweet = json.loads(line)  ## parse JSON text line
+        # get tweet text (or return "" if no text field exists)
+        tweet_text = tweet["text"] if "text" in tweet else ""
+        tweet_text = clean_text(tweet_text)
+        
+        # get list of words from tweet text
+        tweet_words = tweet_text.split(" ")
+        
+        # pass over words once to determine count
+        # of positive and negative words
+        pos_count = 0
+        neg_count = 0
+        for word in tweet_words:
+            if word in score:
+                if score[word] > 0:
+                    pos_count += 1
+                else:
+                    neg_count += 1
+                    
+        # pass over words again adding counts to
+        # each tally
+        for word in tweet_words:
+            if word not in score and len(word) > 2:
+                if word in positive:
+                    positive[word] += pos_count
+                    negative[word] += neg_count
+                else:
+                    positive[word] = pos_count
+                    negative[word] = neg_count
+    
+    # loop over dictionaries to print sentiment scores
+    for word in positive:
+        positive_hits = positive[word]
+        # change negative_hits to 1 to avoid if it's 0
+        # to avoid division by zero error
+        negative_hits = negative[word] if negative[word] > 0 else 1 
+        print word, float(positive_hits)/negative_hits
 
+            
 if __name__ == '__main__':
     main()
--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,44 @@
 import sys
 import json
-import string  ## for list of letters
+
+def get_scored_terms(infile):
+    ''' (file open for reading) -> dict of {str:int}
+    Return dictionary from word list and associated
+    sentiment score in infile.
+    '''
+    # read the file with scores into a dictionary
+    scores = {}  ## initialize empty dictionary
+    for line in infile:
+        term, score = line.split("\t")  ## parse TAB-delimited file
+        scores[term] = int(score)  ## add key-value pair
+    
+    return scores
+
+
+def clean_text(text):
+    ''' (str) -> str
+    Return text after keeping only letters and the
+    space character.
+    '''
+    import string  ## for list of letters
+    
+    # split to characters and replace non-letter 
+    # characters with space
+    letter_list = [c if c in string.letters else " " for c in text]
+    
+    # join again into a string
+    text = "".join(letter_list)
+        
+    # change everything to lowercase and return
+    return text.lower()
+
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
     
-    # read the file with scores into a dictionary
-    scores = {}  ## initialize empty dictionary
-    for line in sent_file:
-        term, score = line.split("\t")  ## parse TAB-delimited file
-        scores[term] = int(score)  ## add key-value pair
-    
+    scores = get_scored_terms(sent_file)
+ 
     # get sub-dictionary of phrases (keys with a space)
     phrase_scores = {key:score for key, score in scores.items() if key.find(" ") != -1}
     
@@ -20,15 +47,11 @@ def main():
         tweet = json.loads(line)  ## parse JSON text line
         # get tweet text (or return "" if no text field exists)
         tweet_text = tweet["text"] if "text" in tweet else ""
-        # only keep letters and the space character
-        tweet_text = "".join([c for c in tweet_text if c in string.letters or c == " "])
+        tweet_text = clean_text(tweet_text)
         
-        # change everything to lowercase
-        tweet_text = tweet_text.lower()
-        
-        tweet_words = tweet_text.split(" ")  ## get list of words
-        # strip punctuation from words
-        
+        # get list of words from tweet text
+        tweet_words = tweet_text.split(" ")
+                
         # get sum of word scores
         sent_score = 0
         for word in tweet_words:
